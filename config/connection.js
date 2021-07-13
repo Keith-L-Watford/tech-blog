fa28690c42a7a84e62fb76785144743ab3bc7fe8
Keith Watford <keith.l.watford@gmail.com>
2021-07-13 14:01:02 -0400

    set up connection.js

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,10 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-        host: 'localho'
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
     }
-)
+);
+
+module.exports = sequelize;
