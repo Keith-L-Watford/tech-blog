c113404bea2408361186767f9b39aea2086352ae
Keith Watford <keith.l.watford@gmail.com>
2021-07-14 09:35:59 -0400

    finished server

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars')
-
+const helpers = require('./utils/helpers')
 const routes = require('./controllers')
+
 const sequelize = require('./config/connection')
+
+const sequelizeStore = require('connect-session-store')(session.store)
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
+
+// TODO set up cookies and session stuff
+const sess = {
+    secret: process.env.secret,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+};
+
+app.use(session(sess))
 
 app.engine('handlebars', hbs.engine);
 app.set('viewq engine', 'handlebars');
