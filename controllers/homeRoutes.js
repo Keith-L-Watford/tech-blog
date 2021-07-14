c073b8a4ada19d993186acf9c32e14f39f2d6c98
Keith Watford <keith.l.watford@gmail.com>
2021-07-14 17:36:08 -0400

    think i did the homeroute

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,6 +48,32 @@ router.get('/post/:id', async (req, res) => {
     }
 });
 
+// profile page
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+       
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        });
 
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/profile');
+        return;
+    }
+    res.render('login');
+});
 
 module.exports = router;
