50ac5d813ce5f0ae65377647e74fb726c66ddd85
Keith Watford <keith.l.watford@gmail.com>
2021-07-14 15:28:02 -0400

    added comment and its relationships

--- models/index.js
+++ models/index.js
@@ -1,13 +1,23 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment') 
 
 User.hasmany(Post, {
     foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+    onDelete: 'CASCADE',
 });
 
 Post.belongsto(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+Post.hasmany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsto(Post, {
+    foreignKey: 'post_id',
+});
+
+module.exports = { User, Post, Comment };
