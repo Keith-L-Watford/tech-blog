93bda3f21743122cd0f6643363b3094784dc28ce
Keith Watford <keith.l.watford@gmail.com>
2021-07-15 09:26:15 -0400

    fixed an error

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -29,7 +29,7 @@ router.delete('/:id', withAuth, async (req, res) => {
             return;
         }
 
-        res.status(200).json(newPost);
+        res.status(200).json(post);
     } catch (err) {
         res.status(500).json(err);
     }
