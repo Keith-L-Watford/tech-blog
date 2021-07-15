61216940534b2d40b077c0d0b180e973da4b7a4d
Keith Watford <keith.l.watford@gmail.com>
2021-07-15 19:58:25 -0400

    missed a line

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
-
 const SequelizeStore = require('connect-session-store')(session.store);
 
 const app = express();
