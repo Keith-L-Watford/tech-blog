e99f2ae6469b033fb592736bebe8d50a61280cc7
Keith Watford <keith.l.watford@gmail.com>
2021-07-15 20:05:08 -0400

    i used connect-session-store instead of connect-session-sequelize

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const helpers = require('./utils/helpers');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-store')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
