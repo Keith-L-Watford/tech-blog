7c960b04ee4a531e33bb10d847bcfb6d3a243e99
Keith Watford <keith.l.watford@gmail.com>
2021-07-15 20:01:47 -0400

    fixed the methods

--- models/index.js
+++ models/index.js
@@ -2,21 +2,21 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment') 
 
-User.hasmany(Post, {
+User.hasMany(Post, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE',
 });
 
-Post.belongsto(User, {
+Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-Post.hasmany(Comment, {
+Post.hasMany(Comment, {
     foreignKey: 'post_id',
     onDelete: 'CASCADE',
 });
 
-Comment.belongsto(Post, {
+Comment.belongsTo(Post, {
     foreignKey: 'post_id',
 });
 
